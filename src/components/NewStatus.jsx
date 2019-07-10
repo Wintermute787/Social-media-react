5bace207499cac971f081f3014b5513e7482d1d3
Student <example@example.com>
2019-07-10 10:19:41 -0700

    Add moments

--- src/components/NewStatus.jsx
+++ src/components/NewStatus.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types'
 import { v4 } from 'uuid';
 import YourStatusList from './YourStatusList';
-import { Route } from 'react-router-dom';
 import styled from 'styled-components';
+import Moment from 'moment';
 
 const Wrapper = styled.div`
 height: 60rem;
+border-radius: 5px;
 background: linear-gradient(45deg, #BBB9B9,#F8F8F8);
     width: 100%;
     box-shadow: 5px 5px 20px;
@@ -24,17 +25,8 @@ function NewStatus(props) {
 
     function handleNewStatusSubmission(event) {
         event.preventDefault();
-        let today = new Date();
-        let hours = parseInt(today.getHours());
-        let date = (today.getMonth()+1)+'/'+today.getDate() +'/' +today.getFullYear();
-        if(hours > 12){
-            hours -= 12;
-        }
-        let time =" at " + hours + ":" + today.getMinutes();
-        let dateTime = date+' '+time;
-        let currentTime = dateTime.toString();
-        props.onNewStatusCreation({names:_names.value, time:currentTime, status: _status.value, id: v4() });
-        allStat.push({names:_names.value, time:currentTime, status: _status.value, id: v4() });
+        props.onNewStatusCreation({names:_names.value, time:new Moment, status: _status.value, id: v4() });
+        allStat.push({names:_names.value, time: new Moment(), status: _status.value, id: v4() });
         _names.value = '';
         _status.value = '';       
         // setFilled(true);
